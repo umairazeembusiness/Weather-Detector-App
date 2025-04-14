eed68624c2ec34e35ff751e9fed3f08ca886e2b6
Umair Azeem <umairazeembusiness@gmail.com>
2025-04-14 17:43:58 +0500

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const weather_body = document.querySelector('.weather-body');
 
 
 async function checkWeather(city){
-    const api_key = "def37c23e78d43c6557acad5250afb54";
+    const api_key = "@@@@@@@@@@@@@@@@@@@";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
 
     const weather_data = await fetch(`${url}`).then(response => response.json());
